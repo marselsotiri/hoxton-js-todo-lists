adf8a1b8a3a8ad974f433fa5955fa3dc0229413d
Marsel <you@example.com>
2021-11-23 15:54:03 +0100

    commit

--- index.js
+++ index.js
@@ -8,22 +8,81 @@ console.log("users: ", window.users);
 
 console.log("todos: ", window.todos);
 
-let message = ''
+// let message = ''
 
-for (const user of users) {
-    message += `Id=${user.id} Hi, ${user.name} from ${user.address.city}!\n`
-}
+// for (const user of users) {
+//     message += `Id=${user.id} Hi, ${user.name} from ${user.address.city}!\n`
+// }
 
-alert(message);
+// alert(message);
 
-let message1 = ''
+// let message1 = ''
 
-let userIdGet=Number(prompt('Enter the user Id you wont to take out'))
+// let userIdGet=Number(prompt('Enter the user Id you wont to take out'))
 
-for (const user of users) {
-   for (const todo of todos){
-    if (userIdGet===user.id && userIdGet===todo.userId) message1 += `This is your user: ${user.username} and the list todo:${todo.title}\n`
-   }
-}
+// for (const user of users) {
+//    for (const todo of todos){
+//     if (userIdGet===user.id && userIdGet===todo.userId) message1 += `This is your user: ${user.username} and the list todo:${todo.title}\n`
+//    }
+// }
 
-alert(message1);
+// alert(message1);
+
+// welcomeUser :: () => undefined
+function welcomeUser() {
+    let welcomeMessage =
+      'Welcome to the AMAZING TODO APP!!!!\n\nAvailable users:\n\n'
+  
+    for (const user of users) {
+      welcomeMessage += `${user.id}: ${user.name} - ${user.address.city}\n`
+    }
+  
+    alert(welcomeMessage)
+  }
+  
+  // getUserId :: () => number
+  function getUserId() {
+    return Number(prompt(`What's your ID?`))
+  }
+  
+  // findUser :: () => User { id, name, username, etc.... }
+  function findUser() {
+    for (const user of users) {
+      if (user.id === userId) {
+        return user
+      }
+    }
+  }
+  
+  function findUserTodos() {
+    let userTodos = []
+  
+    for (const todo of todos) {
+      if (todo.userId === userId) {
+        userTodos.push(todo)
+      }
+    }
+  
+    return userTodos
+  }
+  
+  function alertUserTheirTodos() {
+    let finalMessage = ''
+  
+    finalMessage += `Welcome, ${userFound.username}, here are your todos:\n\n`
+  
+    for (const todo of userTodos) {
+      finalMessage += `- ${todo.title}`
+      if (todo.completed) finalMessage += ` (✅)\n`
+      else finalMessage += ` (❌)\n`
+    }
+  
+    alert(finalMessage)
+  }
+  
+  welcomeUser()
+  let userId = getUserId()
+  let userFound = findUser()
+  let userTodos = findUserTodos()
+  alertUserTheirTodos()
+  
